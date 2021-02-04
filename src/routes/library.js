1c14237358550f2855f3455ec4e4c0036a8a89c1
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-02-04 13:13:51 -0800

    changed start command8

--- src/routes/library.js
+++ src/routes/library.js
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
     res.json(library)
   }
   catch(err){
-    res.status(400).json({message: err})
+    res.status(400).json({message: "there was an error"})
   }
 });
 
