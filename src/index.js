4030b2d60a82e28703ff710f706be32a598e8b97
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-02-04 18:47:01 -0800

    fixed spelling6

--- src/index.js
+++ src/index.js
@@ -27,6 +27,28 @@ app.use(express.json());
 const postRoute = require('./routes/devEdLibrary')
 const authRoute = require("./routes/devEdAuth");
 
+
+
+///
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: 'http://localhost:4040',
+  clientID: 'yS9UtmfaVpEgj4ZZWeOMOs27A89khhzb',
+  issuerBaseURL: 'https://softglow.us.auth0.com',
+  secret: 'fgsdgfsdtrgsdgbdfhjsdfgasdvsdfbsdfgasdvdfgsdfgasdfgvasdvasdfg'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+});
 const { requiresAuth } = require('express-openid-connect');
 
 app.get('/profile', requiresAuth(), (req, res) => {
